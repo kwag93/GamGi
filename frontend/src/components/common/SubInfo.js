60e8b53123a5c4bb0f787922921f3c444e8dad52
bkwag <kwag93@naver.com>
2021-10-25 17:53:41 +0900

    feat: username link 가 필요없어졌기 때문에 삭제

--- frontend/src/components/common/SubInfo.js
+++ frontend/src/components/common/SubInfo.js
@@ -10,12 +10,11 @@ const SubInfoBlock = styled.div`
       margin-top: 1rem;
     `}
   color: ${palette.gray[6]};
-
   span + span:before {
     color: ${palette.gray[4]};
     padding-left: 0.25rem;
     padding-right: 0.25rem;
-    content:'\\B7';
+    content: '\\B7';
   }
 `;
 
@@ -24,7 +23,7 @@ const SubInfo = ({ username, publishedDate, hasMarginTop }) => {
     <SubInfoBlock hasMarginTop={hasMarginTop}>
       <span>
         <b>
-          <Link to={`/@${username}`}>{username}</Link>
+          <span>{username}</span>
         </b>
       </span>
       <span>{new Date(publishedDate).toLocaleDateString()}</span>
